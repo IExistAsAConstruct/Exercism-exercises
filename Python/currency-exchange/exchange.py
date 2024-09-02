1d0eaecaacb072f8dd3edf6cfbeae23b2085738e
IExistAsAConstruct <145796728+IExistAsAConstruct@users.noreply.github.com>
2024-09-02 23:12:10 +0800

    Initial commit

---
+++ Python/currency-exchange/exchange.py
@@ -0,0 +1,90 @@
+"""Functions for calculating steps in exchanging currency.
+
+Python numbers documentation: https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
+
+Overview of exchanging currency when travelling: https://www.compareremit.com/money-transfer-tips/guide-to-exchanging-currency-for-overseas-travel/
+"""
+
+
+
+def exchange_money(budget, exchange_rate):
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+    exchanged_value = budget / exchange_rate
+
+    return exchanged_value
+
+
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: float - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
+    change = budget - exchanging_value
+
+    return change
+
+
+def get_value_of_bills(denomination, number_of_bills):
+    """
+
+    :param denomination: int - the value of a bill.
+    :param number_of_bills: int - total number of bills.
+    :return: int - calculated value of the bills.
+    """
+
+    bill_value = int(denomination * number_of_bills)
+
+    return bill_value
+
+
+def get_number_of_bills(amount, denomination):
+    """
+
+    :param amount: float - the total starting value.
+    :param denomination: int - the value of a single bill.
+    :return: int - number of bills that can be obtained from the amount.
+    """
+
+    bill_number = amount // denomination
+
+    return bill_number
+
+
+def get_leftover_of_bills(amount, denomination):
+    """
+
+    :param amount: float - the total starting value.
+    :param denomination: int - the value of a single bill.
+    :return: float - the amount that is "leftover", given the current denomination.
+    """
+
+    bill_leftover = amount % denomination
+
+    return bill_leftover
+
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+
+    spread_percentage:float = spread / 100
+    exchange_value:float = exchange_rate + (exchange_rate * spread_percentage)
+    exchange_budget:float = exchange_money(budget, exchange_value)
+    bill_count:int = get_number_of_bills(exchange_budget, denomination)
+    value:float = get_value_of_bills(denomination, bill_count)
+
+    return value
---
+++ README.md
@@ -0,0 +1,2 @@
+# Exercism exercises
+ A repository of completed Exercism exercises. A few early ones are missing due to being completed before the formation of this repo.
